9a35e173f4b2c7541c4d28df968f283853875795
WarDog92 <nik.nikulin@bk.ru>
2022-10-24 23:04:07 +1000

    Тестовая сборка

--- src/components/TheFooter.ts
+++
@@ -1,6 +0,0 @@
-import { Vue } from 'vue-class-component';
-
-
-export default class TheFooter extends Vue {
-
-}
--- src/components/TheHeader.ts
+++
@@ -1,6 +0,0 @@
-import { Vue } from 'vue-class-component';
-
-
-export default class TheHeader extends Vue {
-
-}
